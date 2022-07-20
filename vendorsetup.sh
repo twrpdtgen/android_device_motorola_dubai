0f85610ffdf650d5cdeddc624bfe445cd5cea44a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-20 02:58:03 +0200

    dubai: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g200 5G Script version: 2.3.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_dubai-user
+add_lunch_combo omni_dubai-userdebug
+add_lunch_combo omni_dubai-eng
