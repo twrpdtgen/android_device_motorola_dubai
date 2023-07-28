589e76adb6385210608d16da75a562204705efab
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-28 11:16:54 +0200

    dubai: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: motorola edge 30 Script version: 3.0.0

---
+++ omni_dubai.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from dubai device
+$(call inherit-product, device/motorola/dubai/device.mk)
+
+PRODUCT_DEVICE := dubai
+PRODUCT_NAME := omni_dubai
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola edge 30
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dubai_g-user 13 T1RD33.116-33-3 f829db-a0c97 release-keys"
+
+BUILD_FINGERPRINT := motorola/dubai_g/dubai:13/T1RD33.116-33-3/f829db-a0c97:user/release-keys
