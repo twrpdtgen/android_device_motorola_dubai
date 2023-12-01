a4fb9150980fd60dafe4e99113c0588de48a1487
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-12-01 05:14:11 +0100

    dubai: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: motorola edge 30 Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_dubai-user
+add_lunch_combo omni_dubai-userdebug
+add_lunch_combo omni_dubai-eng
